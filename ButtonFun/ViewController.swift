db498f21d65f3728bd49616f004148dd1d7a794a
Bill Goggin <bgoggin@mac.com>
2017-08-05 20:59:34 -0400

    Initial Commit

---
+++ ButtonFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ButtonFun
+//
+//  Created by Bill Goggin on 8/5/17.
+//  Copyright © 2017 Bill Goggin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
